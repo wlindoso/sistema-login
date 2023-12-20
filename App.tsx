906ea8abf1f1232465573525f411ddeb916c4b01
Waldeck Lindoso Jr <wlindoso@gmail.com>
2023-12-20 18:48:43 -0300

    first commit login screen project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import { useState } from "react";
 
-export default function App() {
+const App = () => {
+  const [emailField, setEmailField] = useState<string>("");
+  const [passwordField, setPasswordField] = useState<string>("");
+
+  const handleLoginButton = () => {
+    alert("Email: " + emailField + "\nSenha: " + passwordField);
+  };
+
+  const handleForgetButton = () => {
+    alert("Um email para redefinição de senha.");
+  };
+
+  const handleSignUpButton = () => {
+    alert("Encaminhamento para tela de cadastro.");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <Image
+          source={require("./assets/logo.png")}
+          style={styles.logo}
+        />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <Text style={styles.h1}>Sistema de Login</Text>
+        <Text style={styles.h2}>Bem vindo(a)! Digite seus dados abaixo.</Text>
+
+        <View style={styles.inputArea}>
+          <Text style={styles.inputLabel}>Email</Text>
+          <TextInput
+            style={styles.inputField}
+            placeholder="Digite seu email"
+            placeholderTextColor={"#999999"}
+            value={emailField}
+            onChangeText={(text) => setEmailField(text)}
+            autoCapitalize="none"
+            keyboardType="email-address"
+          />
+        </View>
+
+        <View style={styles.inputArea}>
+          <Text style={styles.inputLabel}>Senha</Text>
+          <TextInput
+            style={styles.inputField}
+            placeholder="******"
+            placeholderTextColor={"#999999"}
+            secureTextEntry
+            value={passwordField}
+            onChangeText={(text) => setPasswordField(text)}
+          />
+        </View>
+
+        <View style={styles.aditionals}>
+          <TouchableOpacity
+            onPress={handleForgetButton}
+            style={styles.forgotBtnArea}>
+            <Text style={styles.forgotBtnText}>Esqueci minha senha</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity
+          onPress={handleLoginButton}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+
+        <View style={styles.signUpArea}>
+          <Text style={styles.signUpText}>Não tem uma conta? </Text>
+          <TouchableOpacity onPress={handleSignUpButton}>
+            <Text style={styles.signUpBtnText}>Cadastre-se</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.footerArea}>
+          <Text style={styles.footerText}>Criado por Lindoso</Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default App;
---
+++ styles.ts
@@ -0,0 +1,87 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 15,
+    paddingTop: 40,
+  },
+  container: {
+    alignItems: "center",
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  h1: {
+    color: "#000",
+    fontSize: 27,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  h2: {
+    color: "#999",
+    fontSize: 15,
+  },
+  inputArea: {
+    width: "100%",
+    paddingTop: 20,
+  },
+  inputLabel: {
+    color: "#777",
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 7,
+  },
+  inputField: {
+    borderWidth: 2,
+    borderRadius: 5,
+    borderColor: "#ddd",
+    fontSize: 15,
+    padding: 10,
+  },
+  aditionals: {
+    width: "100%",
+  },
+  forgotBtnArea: {
+    paddingVertical: 20,
+    alignSelf: "flex-end",
+  },
+  forgotBtnText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#4162b7",
+  },
+  button: {
+    width: "100%",
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "#4162b7",
+  },
+  buttonText: {
+    alignSelf: "center",
+    color: "#fff",
+  },
+  signUpArea: {
+    flexDirection: "row",
+    paddingTop: 30,
+  },
+  signUpText: {
+    fontSize: 13,
+    fontWeight: "bold",
+    color: "#999",
+  },
+  signUpBtnText: {
+    fontSize: 13,
+    fontWeight: "bold",
+    color: "#4162b7",
+    marginLeft: 5,
+  },
+  footerArea: {
+    marginVertical: 30,
+  },
+  footerText: {
+    fontSize: 13,
+    color: "#999",
+  },
+});
